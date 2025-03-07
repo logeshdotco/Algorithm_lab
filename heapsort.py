0d5eb39ed7d180e7c3c54b5471d28a3587bf2b42
logeshdotco <lsvennila143@gmail.com>
2025-03-07 16:13:38 +0530

    heapsort.py

---
+++ heapsort.py
@@ -0,0 +1,32 @@
+def heap_sort(arr):
+    def heapify(arr, n, i):
+        largest = i
+        left = 2 * i + 1
+        right = 2 * i + 2
+
+        if left < n and arr[left] > arr[largest]:
+            largest = left
+
+        if right < n and arr[right] > arr[largest]:
+            largest = right
+
+        if largest != i:
+            arr[i], arr[largest] = arr[largest], arr[i]
+            heapify(arr, n, largest)
+
+    n = len(arr)
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i, 0)
+
+    return arr
+my_list = [19,911,513,84,89,7]
+sorted_list = heap_sort(my_list)
+print("Sorted array:", sorted_list)
+time=[.26,.36,.39,.45,.49,.52]
+plt.plot(my_list,time)
+plt.xlabel("list")
+plt.ylabel("time")
+plt.show()
